0985f909a881af53185204e37126e991e169fb04
Gaban0I <glt037100@gmail.com>
2023-08-25 16:10:28 +0200

    ajout service api

---
+++ src/ApiService.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { API_BASE_URL } from "./global";
+
+export const getPokemonList = async (startIndex, limit) => {
+  const response = await axios.get(
+    `${API_BASE_URL}/pokemon?offset=${startIndex}&limit=${limit}`
+  );
+  return response.data;
+};
+
+export const getPokemonByName = async (name) => {
+  const response = await axios.get(`${API_BASE_URL}/pokemon/${name}`);
+  return response.data;
+};
