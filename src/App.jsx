b038a2425e83df196b78b0be05dabf5855c563bf
Gaban <glt037100@gmail.com>
2023-08-29 01:27:08 +0200

    update rooting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import Home from "./pages/Home";
+import Poketrap from "./pages/Poketrap";
+import Pokedex from "./pages/Pokedex";
 import Header from "./components/Header";
+import Pokemon from "./pages/Pokemon";
 import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
@@ -7,7 +9,9 @@ function App() {
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path="/poketrap" element={<Home />} />
+        <Route path="/poketrap" element={<Poketrap />} />
+        <Route path="/pokedex" element={<Pokedex />} />
+        <Route path="/pokemon/:name" Component={Pokemon}></Route>
         <Route path="*" element={<Navigate to="/poketrap" replace />} />
       </Routes>
     </div>
